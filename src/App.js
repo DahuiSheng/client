349d7561697fa247b99c907a8c9014900bdfe190
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-01-05 18:35:12 +0900

    sidemenu-buttonの装飾

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ function App() {
   return (
     <div className="App">
       <aside className="sidemenu">
-        <h1>Aside</h1>
+        <div className="side-menu-button">
+          <span>
+            +
+          </span>
+          New chat
+        </div>
+
       </aside>
       <section className="chatbox">
         <h1>chatbox</h1>
